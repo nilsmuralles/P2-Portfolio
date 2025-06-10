03fc6962d3ef5b673254cf9b29a472341396d173
nilsmuralles <murallesnils11@gmail.com>
2025-06-09 18:36:22 -0600

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/P2-Portfolio/',
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
